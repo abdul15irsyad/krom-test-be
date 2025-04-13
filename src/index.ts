1df8ba830b2c37ab1e4f42bd5d227aa7058d0409
Irsyad Abdul <abdulirsyad15@gmail.com>
2025-04-13 19:54:24 +0700

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { nodeENV, port } from './config';
 import { router } from './routes/index.route';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded());
 
